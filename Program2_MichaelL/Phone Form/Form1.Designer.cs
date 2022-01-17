7149635857a0407661f041f4a3e119e8d6483b57
Michael Lawrence <michael@mikedevelops.com>
2022-01-17 14:46:33 -0600

    Add files via upload

---
+++ Program2_MichaelL/Phone Form/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Phone_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox_ContactList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox_PhoneInfo = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_serialNum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_model = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_phoneNum = new System.Windows.Forms.TextBox();
+            this.button_DialPhone = new System.Windows.Forms.Button();
+            this.groupBox_SimCard = new System.Windows.Forms.GroupBox();
+            this.button_addSimCard = new System.Windows.Forms.Button();
+            this.button_removeSim = new System.Windows.Forms.Button();
+            this.textBox_simCard = new System.Windows.Forms.TextBox();
+            this.groupBox_ContactInfo = new System.Windows.Forms.GroupBox();
+            this.button_removeContact = new System.Windows.Forms.Button();
+            this.textBox_contactName = new System.Windows.Forms.TextBox();
+            this.button_addContact = new System.Windows.Forms.Button();
+            this.textBox_contactNumber = new System.Windows.Forms.TextBox();
+            this.groupBox_PhoneInfo.SuspendLayout();
+            this.groupBox_SimCard.SuspendLayout();
+            this.groupBox_ContactInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox_ContactList
+            // 
+            this.comboBox_ContactList.FormattingEnabled = true;
+            this.comboBox_ContactList.Location = new System.Drawing.Point(25, 48);
+            this.comboBox_ContactList.Name = "comboBox_ContactList";
+            this.comboBox_ContactList.Size = new System.Drawing.Size(175, 21);
+            this.comboBox_ContactList.TabIndex = 0;
+            this.comboBox_ContactList.SelectedIndexChanged += new System.EventHandler(this.ComboBox_ContactList_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Contact List:";
+            // 
+            // groupBox_PhoneInfo
+            // 
+            this.groupBox_PhoneInfo.Controls.Add(this.label4);
+            this.groupBox_PhoneInfo.Controls.Add(this.textBox_serialNum);
+            this.groupBox_PhoneInfo.Controls.Add(this.label3);
+            this.groupBox_PhoneInfo.Controls.Add(this.textBox_model);
+            this.groupBox_PhoneInfo.Controls.Add(this.label2);
+            this.groupBox_PhoneInfo.Controls.Add(this.textBox_phoneNum);
+            this.groupBox_PhoneInfo.Location = new System.Drawing.Point(249, 12);
+            this.groupBox_PhoneInfo.Name = "groupBox_PhoneInfo";
+            this.groupBox_PhoneInfo.Size = new System.Drawing.Size(200, 157);
+            this.groupBox_PhoneInfo.TabIndex = 1;
+            this.groupBox_PhoneInfo.TabStop = false;
+            this.groupBox_PhoneInfo.Text = "Phone Information";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Serial Number";
+            // 
+            // textBox_serialNum
+            // 
+            this.textBox_serialNum.Location = new System.Drawing.Point(21, 123);
+            this.textBox_serialNum.Name = "textBox_serialNum";
+            this.textBox_serialNum.ReadOnly = true;
+            this.textBox_serialNum.Size = new System.Drawing.Size(157, 20);
+            this.textBox_serialNum.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Model";
+            // 
+            // textBox_model
+            // 
+            this.textBox_model.Location = new System.Drawing.Point(21, 79);
+            this.textBox_model.Name = "textBox_model";
+            this.textBox_model.ReadOnly = true;
+            this.textBox_model.Size = new System.Drawing.Size(157, 20);
+            this.textBox_model.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Phone Number";
+            // 
+            // textBox_phoneNum
+            // 
+            this.textBox_phoneNum.Location = new System.Drawing.Point(21, 36);
+            this.textBox_phoneNum.Name = "textBox_phoneNum";
+            this.textBox_phoneNum.ReadOnly = true;
+            this.textBox_phoneNum.Size = new System.Drawing.Size(157, 20);
+            this.textBox_phoneNum.TabIndex = 1;
+            // 
+            // button_DialPhone
+            // 
+            this.button_DialPhone.Location = new System.Drawing.Point(39, 213);
+            this.button_DialPhone.Name = "button_DialPhone";
+            this.button_DialPhone.Size = new System.Drawing.Size(144, 33);
+            this.button_DialPhone.TabIndex = 1;
+            this.button_DialPhone.Text = "DIAL";
+            this.button_DialPhone.UseVisualStyleBackColor = true;
+            this.button_DialPhone.Click += new System.EventHandler(this.Button_DialPhone_Click);
+            // 
+            // groupBox_SimCard
+            // 
+            this.groupBox_SimCard.Controls.Add(this.button_addSimCard);
+            this.groupBox_SimCard.Controls.Add(this.button_removeSim);
+            this.groupBox_SimCard.Controls.Add(this.textBox_simCard);
+            this.groupBox_SimCard.Location = new System.Drawing.Point(249, 175);
+            this.groupBox_SimCard.Name = "groupBox_SimCard";
+            this.groupBox_SimCard.Size = new System.Drawing.Size(200, 71);
+            this.groupBox_SimCard.TabIndex = 3;
+            this.groupBox_SimCard.TabStop = false;
+            this.groupBox_SimCard.Text = "Sim Card Options";
+            // 
+            // button_addSimCard
+            // 
+            this.button_addSimCard.Location = new System.Drawing.Point(104, 42);
+            this.button_addSimCard.Name = "button_addSimCard";
+            this.button_addSimCard.Size = new System.Drawing.Size(75, 23);
+            this.button_addSimCard.TabIndex = 1;
+            this.button_addSimCard.Text = "Add Sim Card";
+            this.button_addSimCard.UseVisualStyleBackColor = true;
+            this.button_addSimCard.Click += new System.EventHandler(this.Button_addSimCard_Click);
+            // 
+            // button_removeSim
+            // 
+            this.button_removeSim.Enabled = false;
+            this.button_removeSim.Location = new System.Drawing.Point(21, 42);
+            this.button_removeSim.Name = "button_removeSim";
+            this.button_removeSim.Size = new System.Drawing.Size(75, 23);
+            this.button_removeSim.TabIndex = 0;
+            this.button_removeSim.Text = "Remove";
+            this.button_removeSim.UseVisualStyleBackColor = true;
+            this.button_removeSim.Click += new System.EventHandler(this.Button_removeSim_Click);
+            // 
+            // textBox_simCard
+            // 
+            this.textBox_simCard.Location = new System.Drawing.Point(22, 19);
+            this.textBox_simCard.Name = "textBox_simCard";
+            this.textBox_simCard.Size = new System.Drawing.Size(157, 20);
+            this.textBox_simCard.TabIndex = 0;
+            // 
+            // groupBox_ContactInfo
+            // 
+            this.groupBox_ContactInfo.Controls.Add(this.button_removeContact);
+            this.groupBox_ContactInfo.Controls.Add(this.textBox_contactName);
+            this.groupBox_ContactInfo.Location = new System.Drawing.Point(25, 81);
+            this.groupBox_ContactInfo.Name = "groupBox_ContactInfo";
+            this.groupBox_ContactInfo.Size = new System.Drawing.Size(175, 88);
+            this.groupBox_ContactInfo.TabIndex = 4;
+            this.groupBox_ContactInfo.TabStop = false;
+            this.groupBox_ContactInfo.Text = "Selected Contact Information";
+            // 
+            // button_removeContact
+            // 
+            this.button_removeContact.Location = new System.Drawing.Point(39, 52);
+            this.button_removeContact.Name = "button_removeContact";
+            this.button_removeContact.Size = new System.Drawing.Size(95, 22);
+            this.button_removeContact.TabIndex = 0;
+            this.button_removeContact.Text = "Remove Contact";
+            this.button_removeContact.UseVisualStyleBackColor = true;
+            this.button_removeContact.Click += new System.EventHandler(this.Button_removeContact_Click);
+            // 
+            // textBox_contactName
+            // 
+            this.textBox_contactName.Location = new System.Drawing.Point(14, 20);
+            this.textBox_contactName.Name = "textBox_contactName";
+            this.textBox_contactName.ReadOnly = true;
+            this.textBox_contactName.Size = new System.Drawing.Size(144, 20);
+            this.textBox_contactName.TabIndex = 6;
+            // 
+            // button_addContact
+            // 
+            this.button_addContact.Location = new System.Drawing.Point(105, 20);
+            this.button_addContact.Name = "button_addContact";
+            this.button_addContact.Size = new System.Drawing.Size(95, 22);
+            this.button_addContact.TabIndex = 0;
+            this.button_addContact.Text = "Add Contact";
+            this.button_addContact.UseVisualStyleBackColor = true;
+            this.button_addContact.Click += new System.EventHandler(this.Button_addContact_Click);
+            // 
+            // textBox_contactNumber
+            // 
+            this.textBox_contactNumber.Location = new System.Drawing.Point(39, 182);
+            this.textBox_contactNumber.Name = "textBox_contactNumber";
+            this.textBox_contactNumber.Size = new System.Drawing.Size(144, 20);
+            this.textBox_contactNumber.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 259);
+            this.Controls.Add(this.button_addContact);
+            this.Controls.Add(this.textBox_contactNumber);
+            this.Controls.Add(this.groupBox_ContactInfo);
+            this.Controls.Add(this.groupBox_SimCard);
+            this.Controls.Add(this.button_DialPhone);
+            this.Controls.Add(this.groupBox_PhoneInfo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox_ContactList);
+            this.Name = "Form1";
+            this.Text = "Phone Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox_PhoneInfo.ResumeLayout(false);
+            this.groupBox_PhoneInfo.PerformLayout();
+            this.groupBox_SimCard.ResumeLayout(false);
+            this.groupBox_SimCard.PerformLayout();
+            this.groupBox_ContactInfo.ResumeLayout(false);
+            this.groupBox_ContactInfo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_ContactList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox_PhoneInfo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_phoneNum;
+        private System.Windows.Forms.Button button_DialPhone;
+        private System.Windows.Forms.GroupBox groupBox_SimCard;
+        private System.Windows.Forms.TextBox textBox_simCard;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_serialNum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_model;
+        private System.Windows.Forms.Button button_addSimCard;
+        private System.Windows.Forms.Button button_removeSim;
+        private System.Windows.Forms.GroupBox groupBox_ContactInfo;
+        private System.Windows.Forms.Button button_addContact;
+        private System.Windows.Forms.TextBox textBox_contactNumber;
+        private System.Windows.Forms.TextBox textBox_contactName;
+        private System.Windows.Forms.Button button_removeContact;
+    }
+}
+
---
+++ Program2_MichaelL/Phone Form/Mobile.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellPhone
+{
+    class MobilePhoneExceptionMessage : Exception
+    {
+        private string message;
+        public string MESSAGE { get { return this.message; } set { this.message = value; } }
+
+        public MobilePhoneExceptionMessage(string message)
+        {
+            this.message = message;
+        }
+
+        public new string ToString()
+        {
+            return this.message;
+        }
+    }
+
+    struct Contact
+    {
+        public string number;
+        public string name;
+    }
+        
+
+    //Class for the phone device
+    class Mobile
+    {
+         //Constuctor
+        public Mobile(string model = "Generic Model", string serialnumber = "111111111111", string phoneNumber = "1234567890")
+        {
+            this.model = model;
+            this.serialnumber = serialnumber;
+            this.phoneNumber = phoneNumber;
+            this.hasSim = false;
+            this.contactSize = 0;
+            this.simNum = "";
+            this.contactList = new Contact[CONTACT_CAPACITY];
+            for (int i = 0; i < CONTACT_CAPACITY; i++)
+                this.contactList[i] = new Contact();
+            Console.WriteLine("New phone initialized.\n");
+        }
+        
+        //Add contact to list
+        public void addContact(Contact c)
+        {
+            //If the contact list is full, throw an exception;
+            if (this.contactSize >= CONTACT_CAPACITY)
+                throw new MobilePhoneExceptionMessage("Cannot add another contact.  List is full.");
+            else
+            {
+                //Check if number is valid format. if not throw exception.
+                int valid = this.checkNum(c.number);
+                if (valid == -1)
+                    throw new MobilePhoneExceptionMessage("Number is in incorrect format.  Please enter it in XXX-XXX-XXXX format.");
+                else
+                {
+                    this.contactList[this.contactSize] = c;
+                    this.contactSize++;
+                }
+            }
+        }
+        public void addContact(string num, string name)
+        {
+            Contact temp;
+            temp.name = name;
+            temp.number = num;
+            this.addContact(temp);
+        }
+        //Add a sim card
+        public void addSim(string sim)
+        {
+            this.simNum = sim;
+            this.hasSim = false;
+        }
+        //Remove a sim card
+        public void removeSim()
+        {
+            this.simNum = "";
+            this.hasSim = false;
+        }
+        //Get simcard info
+        public string getSimInfo()
+        {
+            return this.simNum;
+        }
+        //Print the contacts list
+        public void printContacts()
+        {
+            for (int i = 0; i < this.contactSize; i++)
+            {
+                Console.WriteLine("Contact {0}: {1}, {2}", i, this.contactList[i].name, this.contactList[i].number);
+            }
+        }
+        //Call a particular person
+        public void callPerson(string num)
+        {
+            //check that number entered is valid or throw exception
+            int valid = this.checkNum(num);
+            if (valid == -1)
+                throw new MobilePhoneExceptionMessage("Cannot dial number.  Not in valid format.  Please enter number in XXX-XXX-XXXX format.");
+            else
+            {
+                bool isInList = false;
+                int person = 0;
+
+                //Look through the contacts list for the number.  If number is found, retrieve user name.
+                for (int i = 0; i < this.contactSize; i++)
+                {
+                    if (this.contactList[i].number == num)
+                    {
+                        isInList = true;
+                        person = i;
+                        break;
+                    }
+                }
+                //If user exists in contact list, calls the person by name.
+                if (isInList)
+                {
+                    Console.WriteLine("Calling: {0}",this.contactList[person].name);
+                }
+                //If not in the list, just call the number
+                else
+                {
+                    Console.WriteLine("Calling: {0}", num);
+                }
+            }
+        }
+        public void callPerson(Contact c)
+        {
+            this.callPerson(c.name);
+        }
+        //Getters for info
+        public string getModel()
+        {
+            return this.model;
+        }
+        public string getSerial()
+        {
+            return this.serialnumber;
+        }
+        public string getphoneNumber()
+        {
+            return this.phoneNumber;
+        }
+        //Remove a contact from list
+        public void removeContact(string name)
+        {
+            bool found = false;
+            //Look through entire list
+            for (int i = 0; i < this.contactSize; i++)
+            {
+                if (this.contactList[i].name == name)
+                {
+                    found = true;
+                }
+                if (found)
+                {
+                    if (i < this.contactSize - 1)
+                    {
+                        this.contactList[i] = this.contactList[i + 1];
+                    }
+                }
+            }
+            if (found)
+                this.contactSize--;
+            else
+                throw new MobilePhoneExceptionMessage("Cannot remove contact from list, non existent.");
+        }
+        public void removeContact(Contact c)
+        {
+            this.removeContact(c.name);
+        }
+        //returns all of the phone's information as a string
+        public new string ToString()
+        {
+            return "Model: " + this.model + "\nSerial Number: " + this.serialnumber + "\nPhone's number: " + this.phoneNumber + "\nSim: " + this.simNum;
+        }
+        //Method to check the number is in a valid format
+        int checkNum(string num)
+        {
+            if (num.Length < 12)
+                return -1;
+
+            bool valid = true;
+            for (byte i = 0; i < num.Length; i++)
+            {
+                switch (i)
+                {
+                    //Check if the number in the digits spots are between 0 and 9
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 8:
+                    case 9:
+                    case 10:
+                    case 11:
+                        if (num[i] < 48 || num[i] > 57)
+                            valid = false;
+                        break;
+                    //Check if the other characters are -
+                    case 3:
+                    case 7:
+                        if (num[i] != '-')
+                            valid = false;
+                        break;
+                    default:
+                        break;
+
+                }
+                if (valid == false)
+                    return -1;
+            }
+            return 0;
+        }
+        
+        //Strings for phone informaiton
+        private string model, serialnumber, simNum, phoneNumber;
+        private bool hasSim;
+        //array for the contact list
+        protected Contact []contactList;
+        //Number of contacts
+        protected int contactSize;
+        //max size of contact list
+        private const int CONTACT_CAPACITY = 255;
+    
+    }
+}
